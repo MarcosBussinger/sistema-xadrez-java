441247f22ea21ffd2e3762fef5cd33805bf5a120
Marcos.Bussinger <marcosbussingernf@gmail.com>
2023-07-10 13:58:44 -0300

    primeira classe

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-
+		Posicao p = new Posicao(01, 02);
+		System.out.println(p);
 	}
 
 }
