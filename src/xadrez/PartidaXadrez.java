985fd3b4abd227e1c41d2b14161bab11250f1622
Marcos.Bussinger <marcosbussingernf@gmail.com>
2023-07-14 11:41:11 -0300

    Coloração do tabuleiro, criação de todas as peças

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,8 +1,10 @@
 package xadrez;
 
-import tabuleiro.Peca;
-import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.pecas.Bispo;
+import xadrez.pecas.Cavalo;
+import xadrez.pecas.Peao;
+import xadrez.pecas.Rainha;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
 
@@ -23,12 +25,43 @@ public class PartidaXadrez {
 		}
 		return mat;
 	}
-	private void novaPosicao(char coluna, int linha, Peca peca) {
+	private void novaPosicao(char coluna, int linha, PecaXadrez peca) {
 		tabuleiro.porPeca(peca, new PosicaoXadrez(coluna, linha).paraPosicao());
 	}
 	
 	private void setupInicial() {
-		novaPosicao('a',8,new Torre(tabuleiro,Cor.WHITE));
-		novaPosicao('e',8,new Rei(tabuleiro,Cor.WHITE));
+		novaPosicao('a', 1, new Torre(tabuleiro, Cor.WHITE));
+		novaPosicao('b', 1, new Cavalo(tabuleiro, Cor.WHITE));
+		novaPosicao('c', 1, new Bispo(tabuleiro, Cor.WHITE));
+        novaPosicao('d', 1, new Rainha(tabuleiro, Cor.WHITE));
+        novaPosicao('e', 1, new Rei(tabuleiro, Cor.WHITE));
+        novaPosicao('f', 1, new Bispo(tabuleiro, Cor.WHITE));
+        novaPosicao('g', 1, new Cavalo(tabuleiro, Cor.WHITE));
+        novaPosicao('h', 1, new Torre(tabuleiro, Cor.WHITE));
+        novaPosicao('a', 2, new Peao(tabuleiro, Cor.WHITE));
+        novaPosicao('b', 2, new Peao(tabuleiro, Cor.WHITE));
+        novaPosicao('c', 2, new Peao(tabuleiro, Cor.WHITE));
+        novaPosicao('d', 2, new Peao(tabuleiro, Cor.WHITE));
+        novaPosicao('e', 2, new Peao(tabuleiro, Cor.WHITE));
+        novaPosicao('f', 2, new Peao(tabuleiro, Cor.WHITE));
+        novaPosicao('g', 2, new Peao(tabuleiro, Cor.WHITE));
+        novaPosicao('h', 2, new Peao(tabuleiro, Cor.WHITE));
+
+        novaPosicao('a', 8, new Torre(tabuleiro, Cor.BLACK));
+        novaPosicao('b', 8, new Cavalo(tabuleiro, Cor.BLACK));
+        novaPosicao('c', 8, new Bispo(tabuleiro, Cor.BLACK));
+        novaPosicao('d', 8, new Rainha(tabuleiro, Cor.BLACK));
+        novaPosicao('e', 8, new Rei(tabuleiro, Cor.BLACK));
+        novaPosicao('f', 8, new Bispo(tabuleiro, Cor.BLACK));
+        novaPosicao('g', 8, new Cavalo(tabuleiro, Cor.BLACK));
+        novaPosicao('h', 8, new Torre(tabuleiro, Cor.BLACK));
+        novaPosicao('a', 7, new Peao(tabuleiro, Cor.BLACK));
+        novaPosicao('b', 7, new Peao(tabuleiro, Cor.BLACK));
+        novaPosicao('c', 7, new Peao(tabuleiro, Cor.BLACK));
+        novaPosicao('d', 7, new Peao(tabuleiro, Cor.BLACK));
+        novaPosicao('e', 7, new Peao(tabuleiro, Cor.BLACK));
+        novaPosicao('f', 7, new Peao(tabuleiro, Cor.BLACK));
+        novaPosicao('g', 7, new Peao(tabuleiro, Cor.BLACK));
+        novaPosicao('h', 7, new Peao(tabuleiro, Cor.BLACK));
 	}
 }
